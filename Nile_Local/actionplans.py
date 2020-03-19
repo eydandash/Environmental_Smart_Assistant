dbfb92ec956d032dd70dd0fef35b12f52aa68fb6
eydandash <esraadandash96@me.com>
2020-03-19 23:03:44 +0000

    Populated the list of acions

--- Nile_Local/actionplans.py
+++ Nile_Local/actionplans.py
@@ -2,104 +2,188 @@
 
 
 actions = [
-    {"Type": "",
+    {"Type": "carbon",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "Understand the UK Net Zero Emissions law",
+     "Source": "https://www.gov.uk/government/news/uk-becomes-first-major-economy-to-pass-net-zero-emissions-law",
+     "Text": "The UK’s 2050 net zero target — one of the most ambitious in the world — was recommended by the "
+             "Committee on Climate Change, the UK’s independent climate advisory body. Net zero means any emissions "
+             "would be balanced by schemes to offset an equivalent amount of greenhouse gases from the atmosphere, "
+             "such as planting trees or using technology like carbon capture and storage"},
+
+    {"Type": "carbon",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "Understand the standards to measure and manage emissions",
+     "Source": "https://eco-act.com/carbon-reporting/how-to-calculate-a-carbon-footprint-for-your-business/",
+     "Text": """The Greenhouse Gas Protocol (GHGP) sets the global standard for measuring, managing and setting 
+     boundaries for reporting GHG emissions. It includes direct and indirect emission sources, categorised into 
+     “scopes” which Defra lists as follows: •	Scope 1 (Direct emissions): Activities owned or controlled by your 
+     organisation that release emissions straight into the atmosphere. They are direct emissions. Examples of scope 1 
+     emissions include emissions from combustion in owned or controlled boilers, furnaces, vehicles; emissions from 
+     chemical production in owned or controlled process equipment. •	Scope 2 (Energy indirect): Emissions being 
+     released into the atmosphere associated with your consumption of purchased electricity, heat, steam and cooling. 
+     These are indirect emissions that are a consequence of your organisation’s activities but which occur at sources 
+     you do not own or control. •	Scope 3 (Other indirect): Emissions that are a consequence of your actions, 
+     which occur at sources which you do not own or control and which are not classed as scope 2 emissions. Examples 
+     of scope 3 emissions are business travel by means not owned or controlled by your organisation, waste disposal, 
+     or purchased materials or fuels. Beyond scope 3 emissions, the GHG hierarchy also provides options to take 
+     additional measures to “substitute, reduce and avoid” GHG emissions. These might include purchasing renewable 
+     energy, carbon offsetting or woodland creation as a carbon sink"""},
+
+    {"Type": "carbon",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "How do I calculate my greenhouse gas emissions?",
+     "Source": "Defra: Department of Energy and Climate Change",
+     "Text": """If Defra has not been able to provide the appropriate spreadsheet for your activity data or you have 
+     overseas operations, we recommend you refer to the emissions factors in the GHG Protocol calculation tools27. 
+     Other sources of emission factors are: • Overseas national emission factors from other Government sources (e.g. 
+     United States Environmental Protection Agency) • National emission factors from other credible sources (e.g. 
+     Universities & Research Institutions) • International emission factors (e.g. EU level, International Energy 
+     Agency) • Global emission factors (e.g. IPCC level: http://www.ipcc-nggip.iges.or.jp/public/2006gl/)"""},
+
+    {"Type": "carbon",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "Where should I report this information?",
+     "Source": "Defra: Department of Energy and Climate Change",
+     "Text": """Quoted companies already report information on environmental matters (to the extent it is necessary 
+     for an understanding of the development, performance or position of the company's business) in their business 
+     review which forms part of their Annual Report and Accounts. They will want to consider if they wish to include 
+     this information there. We encourage organisations to publish their GHG emissions data and supporting 
+     explanations. Where you report your data is a matter of choice: for some companies it may be their annual report 
+     / business review or it may be in a separate corporate responsibility / sustainability report. Organisations 
+     which do not publish such external reports may wish to publicly disclose this information on their website. To 
+     help you maintain a meaningful and consistent comparison of emissions over time, you will need to choose and 
+     report on a base year. Your base year should be: • The earliest year that verifiable emissions data is available 
+     for • Either a single year, or a multi-year average (e.g. 2006-2008)"""},
+
+    {"Type": "carbon",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "Apply your chosen approach",
+     "Source": "Defra: Department of Energy and Climate Change",
+     "Text": """The approach you choose to consolidate your organisation’s GHG emissions must be applied consistently 
+     to all your operations. Financial control approach For each operation you have identified in your organisational 
+     structure you need to identify if you have financial control over that operation. The fourth column in table 1 
+     sets out for each type of accounting category the percentage of GHG emissions which should be included in the 
+     total amount when reporting at the organisational level using the financial control approach. Operational 
+     control approach For each operation you have identified in your organisational structure you need to identify if 
+     you have operational control over that operation. The fifth column in table 1 below sets out for each type of 
+     accounting category the percentage of GHG emissions which should be included in the total amount when reporting 
+     at the organisational level using the operational approach. Equity share approach For each operation you need to 
+     identify your ownership interest in the operation. The third column in table 1 below sets out for each type of 
+     accounting category the percentage of GHG emissions which should be included in the total amount for the 
+     organisational level using the equity share approach."""},
+
+    {"Type": "water",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "Understand your water bill",
+     "Source": "https://www.ofwat.gov.uk/nonhouseholds/yourwaterbill/hownonhousehold/",
+     "Text": """Most non-household customers are charged based on the amount of water they receive through a meter. A 
+     small number of non-household customers do not have a meter and are charged based on the rateable value of their 
+     property. 
+     The amount you pay for your water and sewerage services each year will depend on where you live and how your water 
+     company charges you. 
+     If you receive your water service and your sewerage service from different suppliers you may receive two bills – one 
+     from each company."""},
+
+    {"Type": "water",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "Check for water leaks",
+     "Source": "https://smarterbusiness.co.uk/resources/how-to-save-on-business-water-rates/",
+     "Text": """CCWater reported that 3.1 billion litres of water were lost in Wales and England every day in 2016/7 
+     – an increase of 1%. The organisation has warned suppliers to curb leakage levels in light of the published 
+     figures. Leading suppliers have responded positing their commitment to leakage performance in meeting targets. 
+     CCWater has reported that only four companies missed their targets this year, raising the concern that targets 
+     aren’t “challenging enough”. As pointed out by Tony Smith, chief executive of CCWater, suppliers’ failures to 
+     address leakage issues can have a negative effect on consumer attitudes to water saving. Leakage figures have 
+     been reduced by a third since the mid-1990s and Ofwat has proposed suppliers work towards a 15% leak reduction 
+     between 2020 and 2025. Continued pressure by these organisations means consumers will benefit from the 
+     associated effect improvements will have on their water bills. Viewed within a broader context, the wastefulness 
+     of the leaks highlights the role of all members of the supply chain in reducing leakage and saving water, 
+     which will have both financial and environmental benefits. """},
+
+    {"Type": "water",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "Consider long-term water-saving investments",
+     "Source": "https://smarterbusiness.co.uk/resources/how-to-save-on-business-water-rates/",
+     "Text": """Major cost savings can be gained through the installation of simple devices that may require higher 
+     upfront costs but result in water and cost-savings in the long run. Purchase water-efficient equipment Urinal 
+     controls or waterless urinals Efficient flush toilets Automatic or sensor taps Water-efficient taps and showers 
+     (these will save on both water and energy by minimising the use of heated water) A rainwater harvesting device 
+     Water recycling infrastructure that will allow you to use greywater or rainwater Use a sensored irrigation 
+     system to better control exterior water use Fit your showers and faucets with low-flow restrictors."""},
+
+    {"Type": "water",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "Monitor progress",
+     "Source": "WRAP",
+     "Text": """Office waste disposal will typically provide the following data:
+      􏰀volume of waste collected by stream – office waste is generally collected and measured at site in volume
+      terms, and then mixed with the waste of other offices during a ‘milk-round’ style collection system; however,
+      large offices may have a compactor and a single collection and therefore tonnage data may be available;
+􏰀      waste recovery rates – rates will be different for segregated and mixed waste streams; for mixed wastes,
+      the recovery rate reported will typically be an average of all waste received by a materials recovery facility."""},
+
+    {"Type": "water",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "Importnant measures to start with",
+     "Source": "https://smarterbusiness.co.uk/resources/how-to-save-on-business-water-rates/",
+     "Text": """Educate and inform your employees on the importance and practices of water efficiency and remind them 
+     to save water at every opportunity. They may also have some ideas of their own. Monitor leakage by checking your 
+     water meters at night or when water is not being used. Make sure that relevant personnel know where your supply 
+     pipes run and where the shut-off valves are located. Should a pipe suddenly burst, someone will then be able to 
+     shut off the mains quickly to avoid water wastage. Insulate your pipes to reduce the risk of water freezing and 
+     pipes bursting. Check your monthly water bills and record your consumption so that you’re aware of any spikes in 
+     your tariffs and can identify the cause of them. Use a smart water meter for efficient water consumption data 
+     and analysis. You can use this to set targets for water efficiency. Conduct regular checks for drips, 
+     leaks and other water wastage and encourage your employees to do the same."""},
+
+    {"Type": "waste",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "What is commercial waste",
+     "Source": "https://startups.co.uk/what-is-commercial-waste/",
+     "Text": """Commercial waste is defined as any waste product that is created from commercial activity. So whether 
+     you’re a builder needing to clear construction waste, an office discarding paper documents, or you need to 
+     dispose of plastic created from your business, it’s all categorised in the same way. This also applies for any 
+     business waste you produce from a home-based company. For example, this could be the food waste created if you 
+     run a cake-making business. """},
+
+    {"Type": "waste",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "Understand the Environment Protection Act 1990",
+     "Source": "https://startups.co.uk/what-is-commercial-waste/",
+     "Text": """The legislation which governs waste disposal, amongst other environmental considerations) states that 
+     you have a ‘duty of care’ for the waste you produce. This includes: Ensuring any waste collection providers are 
+     licensed Containing the waste within your control as much as possible Storing it safely and securely Preventing 
+     the waste from causing any damage to the environment or people Having a waste transfer note if you plan to pass 
+     the waste on to another company"""},
+
+    {"Type": "waste",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "For Ecommerce Businesses: Consider your packaging",
+     "Source": "https://www.gwp.co.uk/packaging/",
+     "Text": "Environmental initiatives and sustainable packaging ideas, such as 100% recyclable cushion packaging, "
+             "Eco Bins and material reduction schemes, should all become an integral part of your business. Checkout "
+             "different environmental packaging companies in the UK such as GWP."},
+
+    {"Type": "waste",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
+     "Title": "Choose the best waste management company for your business according to its size.",
+     "Source": "https://startups.co.uk/best-waste-management-companies/",
+     "Text": """So you want to find a new commercial waste provider – what’s next? With such a range of companies to 
+     choose from, it’s important to spend time comparing the offerings from a number of providers, allowing you to 
+     find the one that's best for you. While some companies are best for coverage, others offer the quickest setup or 
+     the most eco-friendly disposal. Compare different costs here."""},
+
+    {"Type": "waste",
      "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
-     "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
-     "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
-     "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
-     "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
-    {"Type": "",
-     "Priority": 1,
-     "Title": "",
-     "Source": "",
-     "Text": ""},
+     "Title": "Set up a waste prevention plan",
+     "Source": "https://www.resourceefficientscotland.com/reduce-waste/prevention-plan",
+     "Text": """The waste prevention plan template is designed as a working document to be updated and reviewed on a 
+     regular basis. It includes all you need to make positive changes, including: Drivers for change – environmental 
+     (reduce landfill), economic (make financial savings), and social (motivate staff) Task assignment – ensuring 
+     each innovation/change has a member of staff responsible for it Baseline and target – that is, where you are now 
+     and where you want to be in future. Resource Efficient Scotland's waste prevention plan is a great tool for 
+     communicating your aims and commitments within your organisation and to the wider community, including 
+     stakeholders and customers. It also acknowledges staff input and paves the way for further improvement. Download 
+     it here."""},
 ]
