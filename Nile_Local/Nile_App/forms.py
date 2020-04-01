2b48823968321aa5c8f60c45bdfb7bf4a1638f7f
eydandash <esraadandash96@me.com>
2020-04-02 00:52:28 +0100

    Added more Validation steps to form fields

--- Nile_Local/Nile_App/forms.py
+++ Nile_Local/Nile_App/forms.py
@@ -1,6 +1,7 @@
 # In this file I define the two different types of forms used across the web application, login form for signing in
 # and registration form for signing up.
-
+import datetime
+import re
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
@@ -9,7 +10,10 @@ from .models import UserDetails
 
 
 # Login Form
-class LoginForm(forms.ModelForm):
+class LoginForm(ModelForm):
+    # This is important since by default the generate input tag has type text
+    password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta():
         model = User
         fields = ['username', 'password']
@@ -28,15 +32,26 @@ class UserForm(UserCreationForm):
     def clean(self):
         # Ensuring that any validation logic in parent classes is maintained
         cleaned_data = super().clean()
+        # Ensuring email1 and email2 have the same values
         e1 = cleaned_data.get("email")
         e2 = cleaned_data.get("email2")
         if e1 != e2:
             raise forms.ValidationError("Email addresses don't match")
-
-
 
 # Registration Form - Extended
 class UserDataForm(ModelForm):
     class Meta():
         model = UserDetails
         fields = ['company_name', 'company_address', 'contact_no']
+
+    def clean(self):
+        # Ensuring that any validation logic in parent classes is maintained
+        cleaned_data = super().clean()
+        # Ensuring the user enters  UK number using Regex as mentioned here
+        # https://stackoverflow.com/questions/16405187/regular-expression-for-uk-mobile-number-python
+        contact_no = cleaned_data.get("contact_no")
+        rule = re.compile(r'^\+?(44)?(0|7)\d{9,13}$')
+        if not rule.search(contact_no):
+            raise forms.ValidationError("Please enter a valid UK number")
+
+
