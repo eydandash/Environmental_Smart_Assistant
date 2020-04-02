f96863707b48b907b261b5471b06ac4363795c96
eydandash <esraadandash96@me.com>
2020-04-02 01:41:59 +0100

    Linked action plans to logged in users

--- Nile_Local/Nile_App/views/actionplan.py
+++ Nile_Local/Nile_App/views/actionplan.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from actionplans import actions
-
+from Nile_App.models import UserDetails, User
 
 def actionplans(request):
-
-    return render(request, 'action_plan.html', {'listOfActions': actions})
+    print(request.user.id)
+    user_details = UserDetails.objects.get(user_id=request.user.id)
+    return render(request, 'action_plan.html', {'list_of_actions': actions, 'user_details': UserDetails})
