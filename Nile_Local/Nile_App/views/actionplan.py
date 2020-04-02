4d2f81fb9d96be202a5ae9ebd5b8b409a90e32ec
eydandash <esraadandash96@me.com>
2020-04-02 01:00:11 +0100

    Refactored action plans, still to be finsihed

--- Nile_Local/Nile_App/views/actionplan.py
+++ Nile_Local/Nile_App/views/actionplan.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from actionplans import actions
 
 
-def index(request):
+def actionplans(request):
+
     return render(request, 'action_plan.html', {'listOfActions': actions})
