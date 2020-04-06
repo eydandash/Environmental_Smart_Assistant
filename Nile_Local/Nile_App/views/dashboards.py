cb899e79af2e71911339d0bd5c6f3adbdd3f4805
eydandash <esraadandash96@me.com>
2020-04-06 05:08:20 +0100

    Connected both Water and Waste Info to their respective charts

--- Nile_Local/Nile_App/views/dashboards.py
+++ Nile_Local/Nile_App/views/dashboards.py
@@ -4,12 +4,13 @@ import time
 
 from django.shortcuts import render
 from django.http import request
-from Nile_App.models import CarbonIndustry
+from Nile_App.models import CarbonIndustry, WaterIndustry, WasteIndustry
 from nvd3 import pieChart
 import json
 from django.core.serializers.json import DjangoJSONEncoder
-def dashboard(request):
 
+
+def dashboard(request):
     # First GRAPH, to compare carbon_intensity per industry per year LINE CHART, Since maps are not subscriptable, I had to format all of the maps which return a map
     # to a list to use the, in graphs. Using maps as included in the official NVD3 Django documentation didn't work.
     # PLEASE NOTE: ind = industry, y = year, ci = carbon intensity
@@ -48,10 +49,53 @@ def dashboard(request):
 
     }
     print(industries_list)
-    # Second Graph, pie chart for now
-    xdata1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    ydata1 = [3, 5, 7, 8, 3, 5, 3, 5, 7, 6, 3, 1]
-    extra_serie = {}
+
+    # Second Graph, same as one but for waste
+    years_list_waste = WasteIndustry.objects.all().values_list('year', flat=True).distinct().order_by('year')
+    print(years_list_waste)
+    industries_list_waste = WasteIndustry.objects.all().values_list('industry', flat=True).distinct()
+    intensities_list_waste = []
+    intensities_list2_waste = []
+    intensities_list3_waste = []
+    for x in WasteIndustry.objects.filter(industry="Water industry").values_list('total_amount', flat=True):
+        y = json.dumps(x, cls=DjangoJSONEncoder)
+        intensities_list_waste.append(float(x) / 1000)
+
+    for x in WasteIndustry.objects.filter(industry="Power industry").values_list('total_amount', flat=True):
+        y = json.dumps(x, cls=DjangoJSONEncoder)
+        intensities_list2_waste.append(float(x) / 1000)
+
+    for x in WasteIndustry.objects.filter(industry="Agriculture, forestry and fishing").values_list('total_amount',
+                                                                                                    flat=True):
+        y = json.dumps(x, cls=DjangoJSONEncoder)
+        intensities_list3_waste.append(float(x) / 1000)
+
+    xdata_waste = years_list_waste
+    ydata_waste = intensities_list_waste
+    ydata2_waste = intensities_list2_waste
+    ydata3_waste = intensities_list3_waste
+    tooltip_date_waste = "%d %b %Y %H:%M:%S %p"
+    extra_serie1_waste = {"tooltip": {"y_start": "", "y_end": "units"},
+                          "date_format": tooltip_date_waste}
+
+    chartdata_waste = {
+        'x': xdata_waste,
+        'name1': 'Water industry', 'y1': ydata_waste, 'extra1': extra_serie1_waste,
+        'name2': 'Power industry', 'y2': ydata2_waste, 'extra2': extra_serie1_waste,
+        'name3': 'Agriculture, forestry and fishing', 'y3': ydata3_waste, 'extra3': extra_serie1_waste
+
+    }
+
+    # Third Graph, pie chart for now
+    xdata1 = WaterIndustry.objects.all().values_list('industry', flat=True)
+    intensities_list_water = []
+    for x in WaterIndustry.objects.all().values_list('amount', flat=True):
+        y = json.dumps(x, cls=DjangoJSONEncoder)
+        intensities_list_water.append(json.loads(y))
+    ydata1 = intensities_list_water
+    extra_serie = {
+        "tooltip": {"y_start": "", "y_end": " Units"}
+    }
     chartdata1 = {
         'x': xdata1,
         'name1': 'series 1', 'y1': ydata1, 'extra1': extra_serie,
@@ -60,7 +104,8 @@ def dashboard(request):
     chartcontainer = 'linechart_container'  # container name
     charttype1 = "pieChart"
     chartcontainer1 = 'piechart_container1'  # container name
-
+    charttype_waste = "lineChart"
+    chartcontainer_waste = 'linechart_container_waste'  # container name
     # pass all graphs and their relevant data to the template at once
     data = {
         'charttype': charttype,
@@ -69,6 +114,9 @@ def dashboard(request):
         'charttype1': charttype1,
         'chartdata1': chartdata1,
         'chartcontainer1': chartcontainer1,
+        'charttype_waste': charttype_waste,
+        'chartdata_waste': chartdata_waste,
+        'chartcontainer_waste': chartcontainer_waste,
         'extra': {
             'x_is_date': False,
             'x_axis_format': '',
@@ -80,7 +128,12 @@ def dashboard(request):
             'x_axis_format': '',
             'tag_script_js': True,
             'jquery_on_ready': False,
-        }
+        },
+        'extra_waste': {
+            'x_is_date': False,
+            'x_axis_format': '',
+            'tag_script_js': True,
+            'jquery_on_ready': True,
+        },
     }
     return render(request, 'generic_dashboard.html', data)
-
