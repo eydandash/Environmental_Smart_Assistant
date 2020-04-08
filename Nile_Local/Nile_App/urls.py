9d7a990d31ef1198da3319099a12f36b73ad0713
eydandash <esraadandash96@me.com>
2020-04-08 03:40:14 +0100

    Linking URLs of newly refactored dashboards

--- Nile_Local/Nile_App/urls.py
+++ Nile_Local/Nile_App/urls.py
@@ -3,11 +3,14 @@
 from django.urls import path
 
 from Nile_App.views import *
-
+from Nile_App.views import carbondash,wastedash,waterdash
 
 urlpatterns = [
     path('', main.index, name='index'),
     path('registration/', register.registration, name='registration'),
     path('actionplan/', actionplan.actionplans, name='actionplan'),
-    path('dashboard/', dashboards.dashboard, name='dashboard')
+    path('dashboard/', dashboards.dashboard, name='dashboard'),
+    path('carbon_dashboard/', carbondash.dashboard, name='carbon_dashboard'),
+    path('water_dashboard/', waterdash.dashboard, name='water_dashboard'),
+    path('waste_dashboard/', wastedash.dashboard, name='waste_dashboard'),
 ]
