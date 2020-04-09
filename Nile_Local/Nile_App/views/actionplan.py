3a3e23b9f5ee3ef8ac2975bf2d2e08b6129cf2fd
eydandash <esraadandash96@me.com>
2020-04-09 16:52:28 +0100

    Minor naming changes

--- Nile_Local/Nile_App/views/actionplan.py
+++ Nile_Local/Nile_App/views/actionplan.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from actionplans import actions
 from Nile_App.models import UserDetails, User
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def actionplans(request):
     print(request.user.id)
     user_details = UserDetails.objects.get(user_id=request.user.id)
-    return render(request, 'action_plan.html', {'list_of_actions': actions, 'user_details': UserDetails})
+    comName = user_details.company_name
+    return render(request, 'action_plan.html', {'list_of_actions': actions, 'user_details': UserDetails, 'company': comName})
