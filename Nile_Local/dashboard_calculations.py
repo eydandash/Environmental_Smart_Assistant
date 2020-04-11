a057761757958cb735ef20cb3e19a9bdd4bc6c73
eydandash <esraadandash96@me.com>
2020-04-11 01:20:54 +0100

    Ecternal file with the basic functions to be applied on data in the views, and edited out of views

---
+++ Nile_Local/dashboard_calculations.py
@@ -0,0 +1,55 @@
+# This file contains all extra preprocessing and calculations that are performed on the data displayed ina nay of the
+# 4 dashboard templates
+
+# Calculating total Carbon cost, https://qz.com/1192753/a-carbon-tax-killed-coal-in-the-uk-natural-gas-is-next/
+from _decimal import Decimal
+from typing import Dict
+
+
+def get_carbon_cost(tonnes):
+    # Using decimal here is important to since multiplication by decimals sabotages precision and produces wrong long results
+    return 18 * Decimal(tonnes)
+
+
+# Calculating the total water usage, based on the number of employees * 50 litres(m3) * 5 days work * 52 weeks
+# found here https://www.south-staffs-water.co.uk/media/1509/waterusebusiness.pdf
+
+
+def get_water_consum(emp):
+    return emp * emp * 5 * 52
+
+
+# Calculating the water cost, depending the supplier, there is no need to classify into business size because the m3
+# price is still the same, found here https://www.businesselectricityprices.org.uk/water-prices/
+
+providers = {
+    'Thames Water': 1.3560,
+    'Severn Trent': 1.5586,
+    'United Utilities': 1.7050,
+    'Northumbrian': 1.2195,
+    'Yorkshire Water': 1.2875,
+    'Anglian Water': 1.3396,
+    'Southern Water': 1.2510,
+    'Wessex Water': 2.2058,
+    'South West': 1.9714,
+    'Business Stream': 2.1442
+}
+
+
+def get_water_cost(emp, supp):
+    consum = get_water_consum(emp)
+    current_rate = providers.get(supp)
+    cost = consum * current_rate
+    recommended_supp = {}
+    for key, value in providers.items():
+        if current_rate > value:
+            recommended_supp.update({key: value})
+            # returns a tuple
+    return cost, recommended_supp
+
+
+# Calculating the waste cost. found here https://startuptoday.co.uk/business-1/how-much-waste-is-produced-in-the-uk/
+
+
+def get_waste_cost(tonnes):
+    return 3500 * tonnes
